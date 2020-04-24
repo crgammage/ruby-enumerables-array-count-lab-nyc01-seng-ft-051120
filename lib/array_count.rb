dbfec33e8877d8ab7d7537f410100c8c858f4f61
Christa Gammage <crgammage@gmail.com>
2020-04-24 15:09:23 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,13 @@
 def count_strings(array)
   i = 0
+  counted_strings = []
   while i < array.length do
     if array[i].class == String
-
+      counted_strings << array[i]
+    end
+    i += 1
   end
+  return counted_strings.count
 end
 
 def count_empty_strings(array)
